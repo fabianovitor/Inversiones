3e6526b964dd78ebfa4717d8483722e85da0fe00
fabianovitor <149432261+fabianovitor@users.noreply.github.com>
2026-05-19 09:08:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="📊 Inversiones FCV",
+    page_title="Inversiones FCV",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="collapsed",
